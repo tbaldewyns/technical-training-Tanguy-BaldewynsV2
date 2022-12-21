ec4ec32168773d7a43e72f3f4a14c9af8c07fba8
TBaldewyns <94040981+tbaldewyns@users.noreply.github.com>
2022-12-21 16:33:53 +0100

    firstCommit

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,9 +2,8 @@
     "name": "Estate",  # The name that will appear in the App list
     "version": "16.0",  # Version
     "application": True,  # This line says the module is an App, and not a module
-    "depends": ["base"],  # dependencies
-    "data": [
-
+    'depends': ['base', 'sale_management', 'calendar', 'hr'],
+    'data': ["views/res_groups.xml", "views/sale_order.xml"
     ],
     "installable": True,
     'license': 'LGPL-3',
