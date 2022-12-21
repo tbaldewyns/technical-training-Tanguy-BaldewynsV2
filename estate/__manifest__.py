b008b2f68e84d3d6209297063dac6e566720aecd
TBaldewyns <94040981+tbaldewyns@users.noreply.github.com>
2022-12-21 16:49:44 +0100

    updateSaleOrderLine

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,12 @@
 {
-    "name": "Estate",  # The name that will appear in the App list
-    "version": "16.0",  # Version
-    "application": True,  # This line says the module is an App, and not a module
+    'name': "estate",
+    'summary': """Project for HELB Student""",
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '16.0.0.1',
+    'license': 'LGPL-3',
     'depends': ['base', 'sale_management', 'calendar', 'hr'],
     'data': ["views/res_groups.xml", "views/sale_order.xml"
-    ],
-    "installable": True,
-    'license': 'LGPL-3',
+    ]
 }
--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -4,3 +4,10 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="Training Date")
     employee = fields.Many2one("hr.employee", string="employee")
+
+@api.model
+def create(self, vals):
+    res = super(CalendarEvent, self).create(vals)
+    # code here to create sale order
+    
+    return res
